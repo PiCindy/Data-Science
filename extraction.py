b8ae9b458ffb0ac258f55b4f18f01ae67e8d5725
Cindy Pereira <36028883+PiCindy@users.noreply.github.com>
2021-05-05 15:05:09 +0200

    Add files via upload

---
+++ extraction.py
@@ -0,0 +1,100 @@
+import re
+import nltk
+import string
+import wptools
+import wikipedia
+import pandas as pd
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+def create_list(q, k):
+    '''
+    Creation of a list of k persons of category q.
+    Input:
+    q (str): category id
+    k (int): nb of persons
+    Output:
+    List of k persons id of category q
+    '''
+    # Creating the SPARQL query
+    query = "select distinct ?item where {?item wdt:P31 wd:Q5; wdt:P106 wd:%s.}" %(q)
+    sparql = SPARQLWrapper("http://query.wikidata.org/sparql")
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    # Getting the results
+    results = sparql.query().convert()
+    return [result['item']['value'].split('/')[-1] for result in results["results"]["bindings"][:k]]
+
+def title_desc(identifier):
+    '''
+    Extraction of title and description of a person's page.
+    Input:
+    identifier (str): person id
+    Output:
+    Title of the page
+    Description of the page
+    '''
+    page = wptools.page(wikibase=identifier)
+    page.get_wikidata()
+    return page.data['title'], page.data['description']
+
+def create_data(persons, category, n):
+    '''
+    Fills a list data with each person info
+    Input:
+    persons (list): list of persons
+    category (str): category of the list
+    n (int): number of sentences per person
+    '''
+    data = []
+    # Finding the type of the category
+    t = ''
+    if category == 'singer' or category == 'writer' or category == 'painter':
+        t = 'A'
+    elif category == 'architect' or category == 'politician' or category == 'mathematician':
+        t = 'Z'
+    # Getting data for every person
+    for p in persons:
+        try:
+            # Getting title and description
+            title, desc = title_desc(p)
+            # Accessing the person page
+            page = wikipedia.page(title, auto_suggest=False)
+            # Removing section names and line breaks in the summary
+            summary = re.sub('==.+==', '', page.content).replace('\n', ' ')
+            # Tokenizing sentences
+            sentences = nltk.sent_tokenize(summary)[:n]
+            # Adding a list with info in data
+            data.append([title, category, t, desc, sentences])
+        # If an exception is found, we cannot have all info and we ignore this person
+        # Problem -> Not k persons in this category
+        # Should replace this person by another one (create lists with more people?)
+        except wikipedia.exceptions.PageError:
+            continue
+        except LookupError:
+            continue
+        except wikipedia.DisambiguationError:
+            continue
+    data.to_csv('data.csv')
+
+def extraction(k=30, n=5):
+    '''
+    Corpus extraction
+    Parameters:
+    k (int): number of persons per category
+    n (int): number of sentences per person
+    '''
+
+    # Creation of the lists of persons
+    singers = create_list("Q177220", k)
+    writers = create_list("Q36180", k)
+    painters = create_list("Q1028181", k)
+    architects = create_list("Q42973", k)
+    politicians = create_list("Q82955", k)
+    mathematicians = create_list("Q170790", k)
+
+    # Completion of data
+    variables = [singers, writers, painters, architects, politicians, mathematicians]
+    categories = ['singer', 'writer', 'painter', 'architect', 'politician', 'mathematician']
+
+    for v, c in zip(variables, categories):
+        create_data(v, c, n)
---
+++ main.py
@@ -0,0 +1,30 @@
+import nltk
+import extraction
+import pandas as pd
+from nltk.corpus import stopwords
+
+def preprocessing(text):
+    """
+    Application of all preprocessing methods on the text.
+    Input:
+    text (string): a Wikipedia summary or Wikidata description
+    Output:
+    processed: the text after preprocessing
+    """
+    # Tokenize the text
+    processed = nltk.word_tokenize(text)
+    # Lowercase the tokens
+    processed = [token.lower() for token in processed]
+    # Remove stop words
+    en_stopwords = stopwords.words('english')
+    processed = [token for token in processed if token not in en_stopwords]
+    return ' '.join(processed)
+
+df = pd.read_csv('data.csv')
+data = pd.DataFrame()
+data['person'] = df['person']
+data['text'] = df['text']
+data['processed_text'] = df['text'].apply(preprocessing)
+data['description'] = df['description']
+data['processed_description'] = df['description'].apply(preprocessing)
+data.to_csv('processed_data.csv')
